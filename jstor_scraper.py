612fbcb1350370b6f17257cc4762f7315983240b
Nathaniel Conroy <nathaniel_conroy@alumni.brown.edu>
2016-07-04 10:15:03 -0400

    Update jstor_scraper.py
    Added a mechanism allowing the user to indicate whether to begin a new scrape or continue a previous one. I also added lines of code allowing the user to indicate where the urls are stored and where the output will be stored.

--- jstor_scraper.py
+++ jstor_scraper.py
@@ -1,19 +1,25 @@
 # Open a text file containing a series of jstor search urls, separated by semicolons.
 # Then read the file and split it into a list of the urls.
-e = open('jstorurls.txt', 'r')
+urls = raw_input("What is the path where the jstor URLs are stored?")
+datafile = raw_input("What is the path where the data will be stored?")
+e = open(urls, 'r')
 urlList = e.read()
 urlList = urlList.split(";")
+
+# Ask the user what record to start at.
+startNumber = raw_input("What number would you like to start at? Starting at '0' will erase any previous results.")
+urlList = urlList[startNumber:]
+if startNumber == 0
+	# Open a file where the results of the scrape will be kept and delete its contents if any.
+	f = open(datafile, 'w')
+	f.close()
 
 import requests
 import bs4
 
-# Open a file where the results of the scrape will be kept and delete its contents if any.
-f = open('jstordataoutput.csv', 'w')
-f.close()
-
 # Cycle through all of the urls and scrape the total number of results for each search term.
 # The script will print the results one by one and also write them into the jstordataoutput.csv file.
-n = 0
+n = startNumber
 for x in urlList:
 	rawhtml = requests.get(x)
 	soup = bs4.BeautifulSoup(rawhtml.text)
@@ -24,7 +30,7 @@ for x in urlList:
 		totalresults = '0'
 	else:
 		totalresults = soup.find("h1").get_text(strip=True)
-	f = open('jstordataoutput.csv', 'a')
+	f = open(datafile, 'a')
 	f.write('"' + totalresults + '","' + x + '"' + '\n')
 	f.close()
 	print(n)
